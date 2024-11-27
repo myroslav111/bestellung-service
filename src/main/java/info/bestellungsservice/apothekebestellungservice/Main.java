7f560f40c6161feba10d736a3b857e1d01aca841
ReReRl1 <roarnga@gmail.com>
2024-11-27 10:12:18 +0100

    AnzeigenBeleg mehrere Variablen zu BigDecimal umgewandelt und ausgaben so bearbeiten das nur 2 Nachkommastellen, zur rundung verwendet werden.

--- src/main/java/info/bestellungsservice/apothekebestellungservice/Main.java
+++ src/main/java/info/bestellungsservice/apothekebestellungservice/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         ApplicationRunner app = new ApplicationRunner();
         app.run();
-        //weitere Bugfixes
+
     }
 }
--- src/main/java/info/bestellungsservice/apothekebestellungservice/utils/AnzeigenBeleg.java
+++ src/main/java/info/bestellungsservice/apothekebestellungservice/utils/AnzeigenBeleg.java
@@ -5,26 +5,33 @@ import info.bestellungsservice.apothekebestellungservice.enums.UserMessagesText;
 import info.bestellungsservice.apothekebestellungservice.logistikzentrum.Warenbestand;
 
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Map;
 
 public class AnzeigenBeleg {
     public static void anzeigenBeleg(Map<String, Integer> produktList, Warenbestand warenbestand){
-        double betragBestellen = 0;
+        BigDecimal gesamtBetragBestellung = BigDecimal.ZERO;
         System.out.println("_____________________________________");
         System.out.println("                Beleg                ");
         System.out.println("_____________________________________");
 
-        for(Map.Entry<String, Integer> entry: produktList.entrySet()){
-            betragBestellen += warenbestand.produkte.get(entry.getKey()).getPreis() * entry.getValue();
-            System.out.println(entry.getKey());
-            System.out.println("                     " + entry.getValue() + "St.  " +
-                    (warenbestand.produkte.get(entry.getKey()).getPreis()
-                            * entry.getValue()) + "€");
+        for (Map.Entry<String, Integer> entry : produktList.entrySet()) {
+            String produktName = entry.getKey();
+            int anzahl = entry.getValue();
+
+            BigDecimal gesamtProduktPreis = warenbestand.produkte.get(produktName).getPreis().multiply(BigDecimal.valueOf(anzahl));
+
+            gesamtBetragBestellung = gesamtBetragBestellung.add(gesamtProduktPreis);
+
+            System.out.println(produktName);
+            System.out.println("                     " + anzahl + "St.  " + gesamtProduktPreis.setScale(2, RoundingMode.HALF_UP) + "€");
         }
 
         System.out.println("_____________________________________");
 
-        String gesamtbetragText = UserMessagesText.GESAMTBETRAG.format(Math.round(betragBestellen * 100.0) / 100.0);
+
+        String gesamtbetragText = UserMessagesText.GESAMTBETRAG.format(gesamtBetragBestellung.setScale(2, RoundingMode.HALF_UP));
         System.out.println(Farbcodes.HELLBLAU.formatText(gesamtbetragText));
     }
 }
